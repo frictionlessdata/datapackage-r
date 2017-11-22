0f7108b929c81e720299e69d0ab1cabfe4f6aefc
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2017-11-22 14:38:18 +0200

    add depedencies and validate function

---
+++ R/validate.R
@@ -0,0 +1,24 @@
+#' validate descriptor
+#' @param descriptor descriptor
+#' @rdname validate
+#' @export
+#' 
+
+# Module API
+
+
+# https://github.com/frictionlessdata/datapackage-js#validate
+
+validate = function (descriptor) {
+  
+  future::future({
+    
+    valid_errors= Package$load(descriptor)
+    
+    names(valid_errors)= c(valid, errors)
+    
+    return (valid_errors)
+    
+  })
+
+  }
