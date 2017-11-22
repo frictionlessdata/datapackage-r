82fdeb38237ac954d7a51c1d32bb862218d7f2c4
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2017-11-22 15:49:38 +0200

    locateDescriptor,retrieveDescriptor, isRemotePath, isUndefined, push functions

--- R/helpers.R
+++ R/helpers.R
@@ -1,6 +1,102 @@
+# Locate descriptor
+
+locateDescriptor = function (descriptor) {
+  
+  # Infer from path/url
+  
+  if (is.character(descriptor)) {
+    
+    basePath = unlist(strsplit(descriptor, '/', simplify = TRUE))
+    basePath = basePath[-lenght(basePath)]
+    basePath = paste(basePath, collapse = "/")
+  
+    # Current dir by default
+  } else {
+    
+    basePath = '.'
+    
+  }
+  
+  return (basePath)
+}
+
+# Retrieve descriptor
+
+retrieveDescriptor = function (descriptor) {
+  
+  if (jsonlite::validate(descriptor)) {
+    
+    descriptor = descriptor
+    
+  }
+  if (is.character(descriptor)) {
+    
+    # Remote
+    if (isRemotePath(descriptor)) {
+      
+      tryCatch({
+        
+        response = httr::GET(descriptor)
+        
+        descriptor = httr::content(response, as = 'text')
+        
+      }, 
+      
+      error = function(e) {
+        
+        message = stringr::str_interp("Can not retrieve remote descriptor '${descriptor}'")
+        
+        DataPackageError$new(message)
+        
+        stop(DataPackageError$new(
+          
+          stringr::str_interp("Can\'t load descriptor at '${descriptor}'"),
+          
+          errors
+          
+        ))
+        
+      })
+      
+      # Local
+      
+    } else {
+      
+      # if (config.IS_BROWSER) {
+      #   message = stringr::str_interp("Local descriptor '${descriptor}' in browser is not supported")
+      #   DataPackageError$new(message)
+      #   
+      # }
+      
+      tryCatch({
+        
+        # TODO: rebase on promisified fs.readFile (async)
+        
+        contents =  readr::read_file(system.file(descriptor, package = "datapackage.r"))
+        
+        return (jsonlite::toJSON(contents))
+      }, 
+      
+      error = function(e) {
+        
+        message = stringr::str_interp("Can not retrieve local descriptor '${descriptor}'")
+        
+        DataPackageError$new(message)
+        
+      })
+      
+      
+    }
+    
+  }
+  
+  DataPackageError$new('Descriptor must be String or Object')
+}
+
 #' is git
 #' @param x url
 #' @rdname is.git
+#' @return TRUE if url is git
 #' @export
 #' 
 
@@ -11,6 +107,7 @@ is.git <- function(x){
 #' is compressed
 #' @param x file
 #' @rdname is.compressed
+#' @return TRUE if file is compressed
 #' @export
 #' 
 
@@ -45,16 +142,20 @@ filepath=function(x){
 }
 
 
-#' get descriptor path
+#' Get descriptor path
+#' 
+#' @description Find descriptor path in directory
 #' 
 #' @usage get.descriptor.path(directory= ".")
 #' 
 #' @param directory A character vector of full path name. The default corresponds to the working directory specified by \code{\link[base]{getwd}}
+#' 
 #' @rdname get.descriptor.path
+#' 
 #' @export
 #' 
 
-get.descriptor.path=function(directory= "."){
+get.descriptor.path = function(directory= "."){
   
   # datapackage.json(descriptor) exists?
   
@@ -71,3 +172,52 @@ get.descriptor.path=function(directory= "."){
   } else message("Descriptor file (datapackage.json) does not exists.")
   
 }
+
+# Miscellaneous
+
+
+#' is Remote Path
+#' 
+#' @param path path
+#' 
+#' @return TRUE if path is remote
+#' @rdname isRemotePath
+#' @export
+#' 
+
+isRemotePath = function (path) {
+  
+  if (!is.character(path)) message("Path should be character")
+  
+  startsWith("http", path)
+  
+}
+
+
+#' Determine if a variable is undefined or NULL
+#' 
+#' @param x variable
+#' 
+#' @return TRUE if variable is undefined
+#' @rdname isUndefined
+#' @export
+#' 
+isUndefined = function(x){
+  
+  if (any(isTRUE( !exists(deparse(substitute(x))) || is.null(x)  ))) TRUE else FALSE
+  
+}
+
+#' Push elements in a list or vector
+#' 
+#' @param x list or vector
+#' @param value value to push in x
+#' 
+#' @rdname push
+#' @export
+#' 
+push = function(x, value){
+  x = append(x,value)
+  return (x)
+}
+
