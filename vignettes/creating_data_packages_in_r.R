d1e7a73be61f48ccdea37c684b8a8acd9ffa6371
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2018-01-29 19:01:37 +0200

    add config and .R file

---
+++ vignettes/creating_data_packages_in_r.R
@@ -0,0 +1,49 @@
+## ---- eval=FALSE, include=TRUE-------------------------------------------
+#  # Install devtools package if not already
+#  install.packages("devtools")
+
+## ---- eval=FALSE, include=TRUE-------------------------------------------
+#  devtools::install_github("frictionlessdata/datapackage.r")
+
+## ---- eval=TRUE, include=TRUE--------------------------------------------
+library(datapackage.r)
+
+## ---- eval=TRUE, include=TRUE--------------------------------------------
+dataPackage = Package.load()
+dataPackage$descriptor['name'] = 'period-table'
+dataPackage$descriptor['title'] = 'Periodic Table'
+dataPackage$commit()
+
+## ---- eval=TRUE, include=TRUE--------------------------------------------
+filepath = 'https://raw.githubusercontent.com/frictionlessdata/example-data-packages/master/periodic-table/data.csv'
+
+schema = tableschema.r::infer(filepath)
+
+# define resources using json text 
+resources = helpers.from.json.to.list(
+  '[{
+	"name": "data",
+	"path": "filepath",
+	"schema": "schema"
+  }]'
+)
+resources[[1]]$schema = schema
+resources[[1]]$path = filepath
+
+# define resources using list object
+resources = list(
+  list( name = "data",
+  path = filepath,
+  schema = schema
+  ))
+
+
+## ---- eval=TRUE, include=TRUE--------------------------------------------
+dataPackage$descriptor['resources'] = resources
+
+## ---- eval=FALSE, include=TRUE-------------------------------------------
+#  dataPackage$save(getwd())
+
+## ---- eval=TRUE, include=TRUE--------------------------------------------
+jsonlite::toJSON(dataPackage$descriptor, pretty = TRUE)
+
