c7d02fcce61b9300cd706b615860f9e7f5d7ffd1
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2018-01-29 16:49:13 +0200

    add error tests

--- tests/testthat/test-errors.R
+++ tests/testthat/test-errors.R
@@ -1,38 +1,38 @@
-# library(datapackage.r)
-# library(testthat)
-# library(foreach)
-# 
-# # Tests
-# 
-# testthat::context("DataPackageError")
-# 
-# 
-# 
-# test_that('should work with one error', {
-#   error = DataPackageError$new('message')
-#   expect_equivalent(error$message, 'message')
-#   expect_equivalent(error$multiple, FALSE)
-#   expect_equivalent(error$errors, list())
-# })
-# 
-# 
-# 
-# test_that('should work with multiple errors', {
-#   errors = list('error1', 'error2')
-#   error = DataPackageError$new('message', errors)
-#   expect_equivalent(error$message, 'message')
-#   expect_equivalent(error$multiple, TRUE)
-#   expect_equivalent(length(error$errors), 2)
-#   expect_equivalent(error$errors[1], 'error1')
-#   expect_equivalent(error$errors[2], 'error2')
-# })
-# 
-# test_that('should be catchable as a normal error', {
-#   tryCatch({
-#     DataPackageError$new('message')
-#   }, error = function(e) {
-#       expect_equivalent(error$message, 'message')
-#     expect_equivalent(methods::is(error,"Error") , TRUE)
-#       expect_equivalent(methods::is(error,"DataPackageError"), TRUE)
-#     })
-#   })
+library(datapackage.r)
+library(testthat)
+
+# Tests
+
+######################################
+testthat::context("DataPackageError")
+######################################
+
+
+test_that('should work with one error', {
+  error = DataPackageError$new('message')
+  
+  expect_equivalent(error$message, 'message')
+  expect_equivalent(error$multiple, FALSE)
+  expect_equivalent(error$errors, list())
+})
+
+test_that('should work with multiple errors', {
+  errors = list('error1', 'error2')
+  error = DataPackageError$new('message', errors)
+  
+  expect_equivalent(error$message, 'message')
+  expect_equivalent(error$multiple, TRUE)
+  expect_equivalent(length(error$errors), 2)
+  expect_equivalent(error$errors[1], list('error1'))
+  expect_equivalent(error$errors[2], list('error2'))
+})
+
+test_that('should be catchable as a normal error', {
+  tryCatch({
+    DataPackageError$new('message')
+  }, error = function(e) {
+    expect_equivalent(error$message, 'message')
+    expect_equivalent(methods::is(error,"Error") , TRUE)
+    expect_equivalent(methods::is(error,"DataPackageError"), TRUE)
+    })
+  })
