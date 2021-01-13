c3f8fe96f72487f1023e85b8af54556308b207ca
kleanthisk10 <koupidis.okfgr@gmail.com>
2021-01-13 03:57:28 +0200

    http->https

--- R/datapackage.r.R
+++ R/datapackage.r.R
@@ -149,7 +149,7 @@
 #' \code{
 #' "licenses": [{
 #' "name": "ODC-PDDL-1.0",
-#' "path": "http://opendatacommons.org/licenses/pddl/",
+#' "path": "https://opendatacommons.org/licenses/pddl/",
 #' "title": "Open Data Commons Public Domain Dedication and License v1.0"
 #' }]
 #' }
